5c1fe1f18c73dde473c8f24af64556365f4f51b9
Alexander Binaei <alex.binaei@carpe.io>
2020-06-05 17:26:26 -0700

    Code TLC. Formated + dropped titles from result.

--- src/Main.elm
+++ src/Main.elm
@@ -4,11 +4,14 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (class)
 import Html.Events exposing (onClick, onInput)
+import Http
 import Json.Decode as Decode
 import Json.Encode as Encode
-import Http
+
+
 
 -- Main
+
 
 main : Program () Model Msg
 main =
@@ -23,26 +26,31 @@ main =
 
 -- Model
 
+
 type alias Model =
     { state : State
     , query : Maybe String
     }
+
 
 type State
     = BuildingRequest
     | RequestSuccess Response
     | RequestFailure Http.Error
 
+
 type alias Response =
     { titles : List String
     , related_pages : List String
     }
+
 
 initialModel : Model
 initialModel =
     { state = BuildingRequest
     , query = Nothing
     }
+
 
 init : () -> ( Model, Cmd Msg )
 init _ =
@@ -52,11 +60,13 @@ init _ =
 
 -- UPDATE
 
+
 type Msg
     = SearchInput String
     | MakeRequest
     | PostReceived (Result Http.Error Response)
     | ClearSearch
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
@@ -68,13 +78,17 @@ update msg model =
             case model.query of
                 Just query ->
                     ( model, post (buildRequest query) )
+
                 Nothing ->
                     ( model, Cmd.none )
 
         PostReceived result ->
             case result of
-                Ok response -> ( { model | state = RequestSuccess response }, Cmd.none )
-                Err error->  ( { model | state = RequestFailure error }, Cmd.none )
+                Ok response ->
+                    ( { model | state = RequestSuccess response }, Cmd.none )
+
+                Err error ->
+                    ( { model | state = RequestFailure error }, Cmd.none )
 
         ClearSearch ->
             ( initialModel, Cmd.none )
@@ -83,39 +97,45 @@ update msg model =
 
 -- HTTP
 
+
 type alias Request =
-    { titles: List String
-    , query: String
+    { titles : List String
+    , query : String
     }
+
 
 post : Request -> Cmd Msg
 post request =
-  Http.post
-  { url = "https://am121f9ih9.execute-api.us-west-2.amazonaws.com/default/v1/tap-in"
-    , body = Http.jsonBody (requestEncoder request)
-    , expect = Http.expectJson PostReceived responseDecoder
-    }
+    Http.post
+        { url = "https://am121f9ih9.execute-api.us-west-2.amazonaws.com/default/v1/tap-in"
+        , body = Http.jsonBody (requestEncoder request)
+        , expect = Http.expectJson PostReceived responseDecoder
+        }
 
-requestEncoder: Request -> Encode.Value
-requestEncoder request=
+
+requestEncoder : Request -> Encode.Value
+requestEncoder request =
     Encode.object
         [ ( "titles", Encode.list Encode.string request.titles )
         , ( "query", Encode.string request.query )
         ]
 
-responseDecoder: Decode.Decoder Response
+
+responseDecoder : Decode.Decoder Response
 responseDecoder =
     Decode.map2 Response
         (Decode.field "titles" (Decode.list Decode.string))
         (Decode.field "related_pages" (Decode.list Decode.string))
 
+
 buildRequest : String -> Request
 buildRequest selected =
-    Request [selected] "in"
+    Request [ selected ] "in"
 
 
 
 -- VIEW
+
 
 view : Model -> Html Msg
 view model =
@@ -129,12 +149,14 @@ view model =
         RequestFailure error ->
             viewRequestFailure error
 
+
 viewBuildingRequest : Html Msg
 viewBuildingRequest =
     div [ class "dropdown" ]
-    [ dropdownHead
-    , dropdownBody
-    ]
+        [ dropdownHead
+        , dropdownBody
+        ]
+
 
 viewRequestSuccess : Response -> Html Msg
 viewRequestSuccess response =
@@ -144,55 +166,60 @@ viewRequestSuccess response =
         , viewResponse response
         ]
 
+
 viewRequestFailure : Http.Error -> Html Msg
-viewRequestFailure error  =
+viewRequestFailure error =
     case error of
         Http.BadUrl string ->
             button [ class "dropdown", onClick ClearSearch ]
-            [ text ("Bad Url: " ++ string ++ "\nTry Again!") ]
+                [ text ("Bad Url: " ++ string ++ "\nTry Again!") ]
 
         Http.Timeout ->
             button [ class "dropdown", onClick ClearSearch ]
-            [ text "Server Timeout, Try Again!" ]
+                [ text "Server Timeout, Try Again!" ]
 
         Http.NetworkError ->
             button [ class "dropdown", onClick ClearSearch ]
-            [ text "Network Error, Try Again!" ]
+                [ text "Network Error, Try Again!" ]
 
         Http.BadStatus int ->
             button [ class "dropdown", onClick ClearSearch ]
-            [ text  ((String.fromInt int) ++ ", Try Again!") ]
+                [ text (String.fromInt int ++ ", Try Again!") ]
 
         Http.BadBody body ->
             button [ class "dropdown", onClick ClearSearch ]
-            [ text ("Bad Body: " ++ body ++ "\nTry Again!") ]
+                [ text ("Bad Body: " ++ body ++ "\nTry Again!") ]
+
 
 dropdownHead : Html Msg
 dropdownHead =
-    p [class "header"] [ text "Search for a Tap In!" ]
+    p [ class "header" ] [ text "Search for a Tap In!" ]
+
 
 dropdownBody : Html Msg
 dropdownBody =
     div [ class "dropdown-body" ]
         [ input [ class "search-box", onInput SearchInput ] []
         , makeRequestButton
-         ]
+        ]
+
 
 makeRequestButton : Html Msg
 makeRequestButton =
     button [ class "button", onClick MakeRequest ] [ text "Make Request!" ]
 
-viewResponse: Response -> Html Msg
+
+viewResponse : Response -> Html Msg
 viewResponse response =
     ul [ class "response" ]
-    [ ul [] ([text "titles:"] ++ (responseItems response.titles))
-    , ul [] ([text "related pages:"] ++ (responseItems response.related_pages))
-    ]
+        [ ul [] ([ text "Related Pages:" ] ++ responseItems response.related_pages) ]
 
-responseItems: List String -> List (Html Msg)
+
+responseItems : List String -> List (Html Msg)
 responseItems items =
-    List.map (responseItem) items
+    List.map responseItem items
 
-responseItem: String -> Html Msg
+
+responseItem : String -> Html Msg
 responseItem item =
-    li [] [text item]
+    li [] [ text item ]
